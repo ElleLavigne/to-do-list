4e4c1de3b8696a7087c7d1d5a49a5ff074265b30
Izaelle <izaelleavrillavigne@gmail.com>
2024-10-24 13:49:35 -0300

    feat style task

--- src/pages/components/Task/index.tsx
+++ src/pages/components/Task/index.tsx
@@ -1,21 +1,31 @@
-import { CheckIcon } from "@radix-ui/react-icons";
-import { CheckBoxRoot, TaskContainer } from "./styles";
+// import { CheckIcon } from "@radix-ui/react-icons";
+import { CheckBoxRoot, Label, TaskContainer } from "./styles";
 import * as Checkbox from "@radix-ui/react-checkbox";
-
+import { Check } from "phosphor-react";
+import { useState } from "react";
 
 export function Task() {
-    return(
-        <TaskContainer>
-            <CheckBoxRoot className="CheckboxRoot" id="c1">
-				<Checkbox.Indicator className="CheckboxIndicator">
-					<CheckIcon />
-				</Checkbox.Indicator>
-			</CheckBoxRoot>
+  const [isChecked, setIsChecked] = useState(false);
 
-			<label className="Label" htmlFor="c1">
-				Accept terms and conditions.
-			</label>
+  return (
+    <TaskContainer>
+      <CheckBoxRoot
+        checked={isChecked}
+        id="c1"
+        onCheckedChange={(newValue: boolean) => {
+          setIsChecked(newValue);
+        }}
+      >
+        <Checkbox.Indicator>
+          <Check size={12} color="#F2F2F2" weight="bold"/>
+          {/* <CheckIcon color="#F2F2F2"/> */}
+        </Checkbox.Indicator>
+      </CheckBoxRoot>
 
-        </TaskContainer>
-    )
+      <Label checked={isChecked} htmlFor="c1">
+        Integer urna interdum massa libero auctor neque turpis turpis semper.
+        Duis vel sed fames integer.
+      </Label>
+    </TaskContainer>
+  );
 }
